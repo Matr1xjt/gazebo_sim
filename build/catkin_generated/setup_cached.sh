52da885be91b041a7cde9beaf1627afd0dc25d6e
matr1xjt <2543182247@qq.com>
2025-03-03 17:37:27 +0800

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/zjt/Desktop/gazebo_sim/devel:/opt/ros/noetic:/opt/ros/humble'
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/humble/lib:/usr/local/lib:/usr/local/lib:/usr/share/gazebo/../../lib/x86_64-linux-gnu/gazebo-11/plugins:/home/zjt/velodyne_plugin/build:/usr/local/cuda-12.1/lib64:/usr/share/gazebo/../../lib/x86_64-linux-gnu/gazebo-11/plugins:/usr/share/gazebo/../../lib/x86_64-linux-gnu/gazebo-11/plugins'
+export PATH='/opt/ros/noetic/bin:/opt/ros/humble/bin:/home/zjt/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:/usr/local/texlive/2022/bin/x86_64-linux:/usr/local/cuda-12.1/bin'
+export PWD='/home/zjt/Desktop/gazebo_sim/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/zjt/Desktop/gazebo_sim/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/zjt/Desktop/gazebo_sim/src:/opt/ros/noetic/share'
