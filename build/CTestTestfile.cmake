7e08ffb45f06518884b88c61ee13a91d6ad53d12
matr1xjt <2543182247@qq.com>
2025-04-01 18:33:36 +0800

    change-0401

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zjt/Desktop/gazebo_sim/src
-# Build directory: /home/zjt/Desktop/gazebo_sim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("gazebo")
--- build/gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zjt/Desktop/gazebo_sim/src/gazebo
-# Build directory: /home/zjt/Desktop/gazebo_sim/build/gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
